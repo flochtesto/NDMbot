e0162b630ffe7756eebb383449861c1bc032013f
flochtesto <flochtesto@gmail.com>
2018-03-21 02:31:39 +0100

    Add files via upload

---
+++ src/plugins/sample.js
@@ -0,0 +1,8 @@
+
+module.exports = function(bot) {
+    // Modify bot here
+    // bot.config // NDMbot Config object
+    // bot.client // discord.js client
+    // bot.queue // Music Queue
+    //console.log(bot);
+};
